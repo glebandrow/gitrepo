91705c0c798909eec67311b41e0c51b134bcfe6f
Gleb Lutsenko <gleblutsenko@yandex.ru>
2024-01-24 12:20:52 +0300

    Добавил workflow для теста

---
+++ code.py
@@ -0,0 +1,6 @@
+# Сюда пишем функцию для решения ДЗ
+def calculate(a, b, c) -> int:
+    return a * b * c
+
+print(calculate(1,2,3))
+
---
+++ testing.py
@@ -0,0 +1,14 @@
+# Здесь пишем код для тестирования функции
+from code import calculate
+
+def test(a,b,c):
+    d = calculate(a,b,c)
+
+    test_d = a * b * c
+
+    assert d==test_d
+
+
+test(1,2,3)
+test(2,4,6)
+test(11,12,2)
